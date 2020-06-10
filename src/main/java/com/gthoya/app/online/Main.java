6b788467fc49b8067ce0dc1bfe33ce11de7646f3
“jacob.gt” <jacob.gt@kakaocorp.com>
2020-06-10 09:10:53 +0900

    chapter02. 객체지향 프로그래밍

--- src/main/java/com/gthoya/app/online/Main.java
+++ src/main/java/com/gthoya/app/online/Main.java
@@ -1,14 +1,15 @@
 package com.gthoya.app.online;
 
+import com.gthoya.app.online.client.Customer;
 import com.gthoya.app.online.discount.condition.impl.PeriodCondition;
 import com.gthoya.app.online.discount.condition.impl.SequenceCondition;
 import com.gthoya.app.online.discount.policy.impl.ext.AmountDiscountPolicy;
 import com.gthoya.app.online.money.Money;
+import com.gthoya.app.online.reservation.Reservation;
+import com.gthoya.app.online.reservation.Screening;
 import com.gthoya.app.online.theater.Movie;
 
-import java.time.DayOfWeek;
-import java.time.Duration;
-import java.time.LocalTime;
+import java.time.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -19,5 +20,9 @@ public class Main {
                         new SequenceCondition(1),
                         new SequenceCondition(10),
                         new PeriodCondition(DayOfWeek.MONDAY, LocalTime.of(10, 0), LocalTime.of(11, 59))));
+
+        Screening screening = new Screening(avatar, 1, LocalDateTime.of(LocalDate.now(), LocalTime.of(10, 30)));
+
+        Reservation reservation = new Reservation(new Customer(), screening, avatar.getFee(), 2);
     }
 }
